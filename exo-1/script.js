f749f611e9d127e3ca9da8af918eedb0ef20f039
Tania Barillaro <tania.barillaro@le-campus-numerique.fr>
2025-01-06 13:10:53 +0100

    Exo 1 - I create variable kelvins and convert his value in celsius degrees in celsius variable

--- exo-1/script.js
+++ exo-1/script.js
@@ -1,2 +1,9 @@
 // you can write js here
 console.log('hello from file');
+
+let kelvins = 294;
+
+// I convert kelvins degrees into celsius degrees
+let celsius = kelvins - 273;
+
+console.log('kelvins : ' + kelvins + ' , ' + 'celsius : ' + celsius);
