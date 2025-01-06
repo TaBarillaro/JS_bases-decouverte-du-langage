084f4255a341b7f3d07b78111c5227692617fa14
Tania Barillaro <tania.barillaro@le-campus-numerique.fr>
2025-01-06 15:17:32 +0100

    I created a variable for the current time and I created a condition so that even if it is Friday after 17 is weekend

--- exo-2/script.js
+++ exo-2/script.js
@@ -3,9 +3,9 @@ console.log('exo-2');
 
 const myDate = new Date();
 
-let dayOfTheWeek = 'We are in the week.';
+const dayOfTheWeek = 'We are in the week.';
 
-let weekend = 'Finally the weekend!';
+const weekend = 'Finally the weekend!';
 
 // la variable currentDay renvoie le jour de la semaine (entre 0 et 6) pour la date donnée, selon le temps local
 let currentDay = myDate.getDay();
@@ -15,3 +15,8 @@ let message = (currentDay === 0 || currentDay === 6) ? weekend : dayOfTheWeek;
 
 console.log(message);
 
+// variable correspondant à l'heure actuelle
+let currentTime = myDate.getHours();
+console.log(currentTime);
+
+message = (currentDay === 0 || currentDay === 6 || (currentDay === 5 && currentTime >= 17)) ? weekend : dayOfTheWeek;
