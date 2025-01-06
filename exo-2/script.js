8b33c2d5e3b7f13d9ac9ad2d6c739ddca130cee0
Tania Barillaro <tania.barillaro@le-campus-numerique.fr>
2025-01-06 15:09:32 +0100

    I check if the current day is part of the week or weekend

--- exo-2/script.js
+++ exo-2/script.js
@@ -1,2 +1,17 @@
 // you can write js here
 console.log('exo-2');
+
+const myDate = new Date();
+
+let dayOfTheWeek = 'We are in the week.';
+
+let weekend = 'Finally the weekend!';
+
+// la variable currentDay renvoie le jour de la semaine (entre 0 et 6) pour la date donnée, selon le temps local
+let currentDay = myDate.getDay();
+
+// je vérifie si aujourd'hui est un jour de la semaine ou s'il est weekend
+let message = (currentDay === 0 || currentDay === 6) ? weekend : dayOfTheWeek;
+
+console.log(message);
+
