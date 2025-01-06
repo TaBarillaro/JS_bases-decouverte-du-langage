0a2a4fc0ed5d5c13582c5e42df1ce9b161aa51ca
Tania Barillaro <tania.barillaro@le-campus-numerique.fr>
2025-01-06 16:54:58 +0100

    I create playerInput variable, the function and I transform the variable in lower case

--- exo-3/exo3.js
+++ exo-3/exo3.js
@@ -1,2 +1,18 @@
 // you can write js here
 console.log('exo-3');
+
+// 1.Je crée une variable que contient la question à afficher
+var playerInput = prompt("Your choice ( rock, paper, scissors) ?");
+
+// 2.Je crée la fonction pour faire jouer les joueurs
+function getPlayerChoice(playerInput) {
+    // 3.je transform la variable pour l'avoir en minuscule
+    playerInput.toLowerCase();
+
+    let playerChoise;
+    if (playerChoise === "rock" || playerChoise === "paper" || playerChoise === "scissor") {
+        
+    }
+}
+
+
