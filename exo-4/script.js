13b0ce2bbf32dd9534142041f9e8cc490678f1a8
Tania Barillaro <tania.barillaro@le-campus-numerique.fr>
2025-01-07 13:33:00 +0100

    method to replace elements with a new element in the array

--- exo-4/script.js
+++ exo-4/script.js
@@ -24,3 +24,6 @@ console.log(secretMessage);
 secretMessage.unshift('Programming');
 console.log(secretMessage);
 
+// 6.Utiliser une méthode pour remplacer la série de cinq mots : 'get', 'right', 'the', 'first', 'time' par un seul mot : 'know'
+secretMessage.splice(5, 5, 'know');
+console.log(secretMessage);
