9cfc2b62fe8428a745ebab3640535665d6b1c9d7
Tania Barillaro <tania.barillaro@le-campus-numerique.fr>
2025-01-07 13:12:11 +0100

    Exo 4 - methode to remove the last element from the array

--- exo-4/script.js
+++ exo-4/script.js
@@ -3,3 +3,10 @@
 console.log('exo-4');
 
 var secretMessage = ["Learning", "isn't", "about", "what", "you", "get", "easily", "the", "first", "time,", "it's", "about", "what", "you", "can", "figure", "out.", "-2015,", "Chris", "Pine,", "Learn", "JavaScript"];
+
+// 1.Utiliser une méthode pour enlever le dernier élément du tableau secretMessage
+secretMessage.pop();
+console.log(secretMessage);
+
+
+
