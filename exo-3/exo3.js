5d4122b2a0aa3de065b912e91b2452b211b6ef08
Tania Barillaro <tania.barillaro@le-campus-numerique.fr>
2025-01-07 10:07:10 +0100

    Exo 3- function to the computer choice

--- exo-3/exo3.js
+++ exo-3/exo3.js
@@ -1,21 +1,42 @@
 // you can write js here
 console.log('exo-3');
+const message = getElementById('message');
 
 // 1.Je crée une variable que contient la question à afficher
 var playerInput = prompt("Your choice ( rock, paper, scissors) ?");
 
-// 2.Je crée la fonction pour faire jouer les joueurs
+// 2.Je crée la fonction pour faire jouer le joueur
 function getPlayerChoice(playerInput) {
     // 3.je transform la variable pour l'avoir en minuscule
     playerInput.toLowerCase();
 
-    // 4.condition pour vérifier la choix des joueurs
+    // 4.condition pour vérifier le choix du joueur
     let playerChoise;
-    if (playerChoise === "rock" || playerChoise === "paper" || playerChoise === "scissor") {
+    if (playerChoise === "rock" || playerChoise === "paper" || playerChoise === "scissors") {
         return playerInput;
     } else {
-        console.log('Error: You must choice between rock, paper or scissor');
+        console.log('Error: You must choice between rock, paper or scissors');
     }
 }
+// getPlayerChoice();
+
+// 5. Fonction pour le choix de l'ordinateur
+function getComputerChoice() {
+    let computerChoice = Math.floor(Math.random() * 2);
+    // return computerChoice;
+
+    if (computerChoice === 0) {
+        computerChoice = "rock";
+    } else if (computerChoice === 1) {
+        computerChoice = "paper";
+    } else {
+        computerChoice = "scissors";
+    }
+
+    return computerChoice;
+}
+// getComputerChoice();
+
+
 
 
