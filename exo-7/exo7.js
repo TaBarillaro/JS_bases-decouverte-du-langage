6b9346ef201539d3f8779e8967bdbe91b5b1c02a
Tania Barillaro <tania.barillaro@le-campus-numerique.fr>
2025-01-08 16:35:35 +0100

    Exo 7 - Create an object translationOfType and add it to jsonDatas

--- exo-7/exo7.js
+++ exo-7/exo7.js
@@ -1,3 +1,18 @@
 console.log("exo-7");
 
 console.log(jsonDatas);
+
+// 2 Créer un objet qui servira à traduire les types.
+const translationOfType = {
+    "car": "voiture",
+    "house": "maison",
+    "game": "jeux",
+    "videoGame": "jeux video",
+    "show": "spectacle"
+}
+
+// 3 Ajouter une nouvelle clé aux objets existants pour stocker le type traduit, sans modifier le fichier data.js. Répéter l'étape 1 pour vérifier le type traduit.
+jsonDatas.forEach((item) => {
+    item.translation = translationOfType[item.type];
+});
+console.log(jsonDatas);
